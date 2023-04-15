da5c861f3bfa830f685ca9b9e6c8760b589e7681
Andrei <46579508+Andreisgl@users.noreply.github.com>
2023-04-15 15:23:47 -0300

    Update model_viewer.py

--- model_viewer.py
+++ model_viewer.py
@@ -1,11 +1,19 @@
 import os
 
 cwd = "./"
-file_path = "FIG00H00.ANM"
-ANM_file = ""
+file_name = 'FIG00H00.ANM'
+file_data = []
 
-file_path = os.path.join(cwd, file_path)
+file_path = os.path.join(cwd, file_name)
 
+lines_to_read = 784
+start_offset = 0
 
-with open(file_path) as ANM:
-    ANM_file = ANM.read()
+with open(file_path, 'rb') as file:
+    file.seek(start_offset)
+    for index in range(0, lines_to_read*16, 4):
+        data = file.read(4)
+        data = int.from_bytes(data, byteorder = "little")
+        file_data.append(data)
+
+pass
